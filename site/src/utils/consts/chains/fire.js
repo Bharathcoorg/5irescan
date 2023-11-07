ed5e57405b273a5f2145854deac9b7183ba6f681
Bharathcoorg <Bharathcoorg7@gmail.com>
2023-11-07 22:01:40 +0530

    Initial Commit

---
+++ site/src/utils/consts/chains/fire.js
@@ -0,0 +1,18 @@
+import { ReactComponent as Fire } from "../../../components/icons/edgeware.svg";
+import { governanceModules } from "./modules";
+
+const fire = {
+  name: "5irechain",
+  icon: <Fire style={{ width: '20px'}}/>,
+  identity: "fire",
+  value: "fire",
+  chain: "fire",
+  symbol: "5IRE",
+  decimals: 18,
+  chainIcon: "fire",
+  color: "#6431EC",
+  colorSecondary: "rgba(100, 49, 236, 0.1)",
+  buttonColor: "#6431EC",
+};
+
+export default fire;
